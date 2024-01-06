2f39049990ecaa2306cac7b51a05aeef3abbf263
Joao <dev.joaogustavo.dev@gmail.com>
2024-01-06 14:08:48 -0300

    created feature for bolding about section's topics's text's keywords

--- app/ui/About/About.tsx
+++ app/ui/About/About.tsx
@@ -33,7 +33,7 @@ function About({
         }
       )}`}
     >
-      <div className="w-full max-w-[1115px] pt-[110px] min-h-screen m-auto">
+      <div className="w-full max-w-[1115px] py-[110px] min-h-[679px] m-auto">
         <Title
           text={"Não conhece a"}
           span={"Burger?!"}
@@ -41,11 +41,30 @@ function About({
         />
         <div className="w-full grid grid-cols-3 items-center self-center mt-[100px]">
           {[
-            { title: "Praticidade", img: practicalIcon, p: "1" },
-            { title: "Agilidade & Compromisso", img: deliveryIcon, p: "2" },
-            { title: "Bem-Estar", img: wellBeingIcon, p: "3" },
+            {
+              title: "Praticidade",
+              img: practicalIcon,
+              p: "A Burger é uma franquia de restaurante que te permite aproveitar o melhor do fast food. Que tal um hambúrguer? Basta alguns cliques e isso será possível sem sair do conforto da sua casa! ",
+              keywords: ["Burger", "melhor", "fast", "food.", "alguns", "cliques", "conforto", "da", "sua", "casa."]
+            },
+            {
+              title: "Agilidade & Compromisso",
+              img: deliveryIcon,
+              p: "Vishhh... Tá naquela correria diária? Não se preocupa. A Burger conta com uma equipe qualificada que se compromete em te entregar sempre o melhor com um tempo sempre bem menor!",
+              keywords: ["correria", "diária?", "Burger", "equipe", "qualificada", "compromete", "entregar", "o", "melhor", "tempo", "sempre", "bem", "menor!"]
+            },
+            {
+              title: "Bem-Estar",
+              img: wellBeingIcon,
+              p: "A Burger tem as suas missões! Não basta fornecer as melhores comidas. Mas é preciso também ser responsável com o meio ambiente e compromissado com o bem-estar social! Conta com a Burger!",
+              keywords: ["Burger", "missões!", "melhores", "comidas.", "responsável", "meio", "ambiente", "compromissado", "bem-estar", "social", "Burger!"]
+            },
           ].map((topic, i) => (
-            <Topics key={"topic-" + i} properties={topic} color={theme.textColor} />
+            <Topics
+              key={"topic-" + i}
+              properties={topic}
+              color={theme.textColor}
+            />
           ))}
         </div>
       </div>
--- app/ui/About/Topics.tsx
+++ app/ui/About/Topics.tsx
@@ -5,9 +5,12 @@ function Topics({
   properties,
   color,
 }: {
-  properties: { title: string; img: StaticImageData; p: string };
+  properties: { title: string; img: StaticImageData; p: string; keywords: Array<string> };
   color: string;
 }) {
+
+  const subStringArr = properties.p.split(' ');
+
   return (
     <div className=" max-w-[320px] flex flex-col items-center gap-[15px] m-auto">
       <h1 className={color + " text-center text-[24px] font-bold"}>
@@ -18,7 +21,11 @@ function Topics({
         alt={"Imagem representativa de " + properties.title}
         className="w-full max-w-[120px]"
       />
-      <p className="text-[#434343] text-center">{properties.p}</p>
+      <p className="text-[#434343] text-center">
+        {subStringArr.map((elem, i) => properties.keywords.includes(elem)
+        ? (<span key={"bold-string-" + i} className="font-semibold">{elem + ' '}</span>)
+        : (<>{elem + ' '}</>))}
+      </p>
     </div>
   );
 }
